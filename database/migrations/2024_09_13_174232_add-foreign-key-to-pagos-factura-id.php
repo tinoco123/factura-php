41410b2d1756c8e5ef52468d3ed4505c3d405f26
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2024-09-13 12:06:21 -0600

    Agregar llave foranea para identificar factura desde un pago

---
+++ database/migrations/2024_09_13_174232_add-foreign-key-to-pagos-factura-id.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pagos', function (Blueprint $table) {
+            $table->unsignedBigInteger("factura_id");
+            $table->foreign("factura_id")->references("id")->on("facturas");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pagos', function (Blueprint $table) {
+            $table->dropForeign(['factura_id']);
+            $table->dropColumn('factura_id');
+        });
+    }
+};
